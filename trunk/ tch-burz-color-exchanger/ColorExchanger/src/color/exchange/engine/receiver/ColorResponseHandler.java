65146b5c211b8837147b88acbd678805b7e2ae34
Tomasz.Chrul <Tomasz.Chrul@0ac032fe-b092-ab85-d805-f88ef7be84b7>
2011-01-20 20:22:06 +0000

    code cleanup + javadoc + zmiany struktury pakietow

--- trunk/ tch-burz-color-exchanger/ColorExchanger/src/color/exchange/engine/receiver/ColorResponseHandler.java
+++ trunk/ tch-burz-color-exchanger/ColorExchanger/src/color/exchange/engine/receiver/ColorResponseHandler.java
@@ -1,4 +1,4 @@
-package color.exchange.engine;
+package color.exchange.engine.receiver;
 
 import java.awt.Color;
 import java.io.IOException;
@@ -7,10 +7,22 @@ import java.net.Socket;
 
 import color.exchange.ui.event.ColorQueue;
 
+/**
+ * klasa realizujaca polaczenie TCP z klientem. Klasa implementuje interfejs
+ * Runnable w celu umozliwienia dzialania w tle jako watek
+ * 
+ */
 public class ColorResponseHandler implements Runnable {
 
+    /**
+     * gniazdo komunikacji z klientem
+     */
     private final Socket client;
 
+    /**
+     * @param client
+     *            gniazdo komunikacji z klientem
+     */
     public ColorResponseHandler(Socket client) {
 	super();
 	this.client = client;
@@ -19,16 +31,27 @@ public class ColorResponseHandler implements Runnable {
     public void run() {
 	ObjectInputStream input;
 	try {
+	    // tworzymy strumien do odczytu danych od klienta
 	    input = new ObjectInputStream(client.getInputStream());
+	    // odczytujemy jaki kolor przyslal klient -> dane przyslane
+	    // bezposrednio
+	    // sa zamieniane na obiekt klasy Color (wszystko dzieki klasie
+	    // ObjectInputStream)
 	    Color color = (Color) input.readObject();
+	    // dodajemy odebrany kolor do kolejki z kolorami, dane z kolejki sa
+	    // odczytywane przez oddzielny watek zmieniajacy kolory w GUI
 	    ColorQueue queue = ColorQueue.getInstance();
 	    queue.putColor(color);
+	    // zamykamy strumien
 	    input.close();
+	    // zamykamy gniazdo
 	    client.close();
 	} catch (IOException e) {
+	    // cos poszlo nie tak
 	    System.out.println(this.getClass() + "   " + e.getClass() + "   "
 		    + e.getMessage());
 	} catch (ClassNotFoundException e) {
+	    // cos poszlo nie tak
 	    System.out.println(this.getClass() + "   " + e.getClass() + "   "
 		    + e.getMessage());
 	}
---
+++ trunk/ tch-burz-color-exchanger/ColorExchanger/src/color/exchange/engine/sender/TcpColorSender.java
@@ -0,0 +1,52 @@
+package color.exchange.engine.sender;
+
+import java.awt.Color;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+/**
+ * klasa do komunikacji odbiorca koloru przez gniazdo TCP
+ * 
+ */
+public class TcpColorSender {
+    /**
+     * gniazdo do komunikacji
+     */
+    private Socket socket;
+
+    /**
+     * @param address
+     *            adres ip maszyny na ktorej dziala program odbierajcy kolory
+     * @param port
+     *            port
+     * @throws IOException
+     */
+    public void start(String address, Integer port) throws IOException {
+	socket = new Socket(address, port);
+    }
+
+    /**
+     * metoda przesylajaca kolor do obdiorcy
+     * 
+     * @param r
+     *            wart r koloru
+     * @param g
+     *            wart g koloru
+     * @param b
+     *            wart b koloru
+     * @throws IOException
+     */
+    public void sendColor(int r, int g, int b) throws IOException {
+	// tworzymy strumien do przeslania koloru (korzystajac z gniazda)
+	ObjectOutputStream output = new ObjectOutputStream(socket
+		.getOutputStream());
+	// zapisujemy obiekt do strumienia
+	output.writeObject(new Color(r, g, b));
+
+	// zamykamy strumien
+	output.close();
+	// zamykamy gniazdo
+	socket.close();
+    }
+}
